d98a6fbd9580b4528a50c8a5c8e9df17c0f75bdf
Andreas Abel <andreas.abel@ifi.lmu.de>
2023-05-02 19:18:40 +0200

    Update scripts to move /setup -> /

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  "**/!(*test).{ts,json}": [
-    "eslint --cache --fix",
-    () => "npm run bundle",
-    () => "git add setup/dist/ setup/lib/",
+  '**/!(*test).{ts,json}': [
+    'eslint --cache --fix',
+    () => 'npm run bundle',
+    () => 'git add dist/ lib/'
   ],
-  "**/*.ts": () => "tsc -p tsconfig.json",
-  "**/*.{js,ts,json,md}": "prettier --write",
+  '**/*.ts': () => 'tsc -p tsconfig.json',
+  '**/*.{js,ts,json,md}': 'prettier --write'
 };
